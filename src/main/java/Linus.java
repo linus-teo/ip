902756d4c7dc7288fa6732514d08c7f122a2d476
Linus Teo <linus@isumi.com>
2026-08-18 22:02:06 +0800

    Rename chatbot to Linus

---
+++ src/main/java/Linus.java
@@ -0,0 +1,9 @@
+public class Linus {
+    public static void main(String[] args) {
+        String banner = " _     _                 \n"
+                + "| |   (_)_ __  _   _ ___ \n"
+                + "| |   | | '_ \\| | | / __|\n"
+                + "| |___| | | | | |_| \\__ \\\n"
+                + "|_____|_|_| |_|\\__,_|___/\n";
+    }
+}
