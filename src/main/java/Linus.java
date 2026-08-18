be13338b9241a562e454cdbb7d0db045e8472d5b
Linus Teo <linus@isumi.com>
2026-08-18 22:28:30 +0800

    Add greet user and exit functionality (Level 0)

--- src/main/java/Linus.java
+++ src/main/java/Linus.java
@@ -1,9 +1,27 @@
 public class Linus {
+    public static final String HORIZONTAL_LINE = "____________________________________________________________";
+    public static final String BANNER = " _     _                 \n"
+            + "| |   (_)_ __  _   _ ___ \n"
+            + "| |   | | '_ \\| | | / __|\n"
+            + "| |___| | | | | |_| \\__ \\\n"
+            + "|_____|_|_| |_|\\__,_|___/\n";
+
     public static void main(String[] args) {
-        String banner = " _     _                 \n"
-                + "| |   (_)_ __  _   _ ___ \n"
-                + "| |   | | '_ \\| | | / __|\n"
-                + "| |___| | | | | |_| \\__ \\\n"
-                + "|_____|_|_| |_|\\__,_|___/\n";
+        Linus chatbot = new Linus();
+        chatbot.hello();
+        chatbot.bye();
+    }
+
+    public void hello() {
+        System.out.println(Linus.HORIZONTAL_LINE);
+        System.out.println(Linus.BANNER);
+        System.out.println("Hello! My name is Linus.");
+        System.out.println("How may I help you today?");
+        System.out.println(Linus.HORIZONTAL_LINE);
+    }
+
+    public void bye() {
+        System.out.println("Bye! Hope to see you again soon!");
+        System.out.println(Linus.HORIZONTAL_LINE);
     }
 }
